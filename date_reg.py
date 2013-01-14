22946d83df40bfbc8d2ea65ffc301b8ed88303d7
foreigner92 <foreigner.web@gmail.com>
2013-01-14 22:00:47 +0300

    Branch commit

--- date_reg.py
+++ date_reg.py
@@ -1,29 +1,32 @@
-from datetime import datetime
+import datetime
 import time
 import re
 from exceptions import DateException
 
 
-def check_date(date_string):
-    pattern = re.compile('(?P<day>\d{1,2})[.](?P<month>\d{1,2})[.](?P<year>\d{1,4})')
+def check_date(date_string, delimiter='.', order='straight'):
+    
+    regexp = r'(?P<day>\d{1,2})[' + \
+                delimiter + r'](?P<month>\d{1,2})[.](?P<year>\d{1,4})'
+    pattern = re.compile(regexp)
     mat = pattern.match(date_string)
-    if int(mat.group('year')) >= 1:
-        if int(mat.group('year'))/4 == int(mat.group('year'))/4.0\
-          and int(mat.group('year'))/100 != int(mat.group('year'))/100.0:
-            visokosn=1
+    year, day, month = ('', '', '')
+    if mat:
+        year = mat.group('year')
+        if order == 'straight':
+            day = mat.group('day')
+            month = mat.group('month')
         else:
-            visokosn=0
-        if int(mat.group('month')) in (1, 3, 5, 7, 8, 10, 12)\
-          and int(mat.group('day')) in (1-31)\
-          or int(mat.group('month')) in (4,6,9,11)\
-          and int(mat.group('day')) <= 30 or int (mat.group('month')) == 2\
-          and visokosn == 1 and int(mat.group('day')) <= 29\
-          or int (mat.group('month')) == 2 and visokosn == 0\
-          and int(mat.group('day')) <= 28:
-            return time.strptime(date_string, "%d.%m.%Y")
-        else:
-            raise DateException("Invalid Format!")
+            day = mat.group('month')
+            month = mat.group('day')
     else:
-        raise DateException("Invalid Format!")
+        raise DateException('Bad Date String')
+    if len(mat.group('year')) == 2 and mat.goup('year')[0] == '0':
+        year = '20' + year
+    try:
+        date_obj = datetime.date(int(year), int(month), int(day))
+    except ValueError:
+        raise DateException("Date is out of calendar range")
+    return date_obj
 
 
